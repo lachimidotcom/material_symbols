bcec8f98fb0714af6e15a6e75ac647d107fae9e6
lachimi <officiallachimi@gmail.com>
2022-09-03 11:46:04 +0530

    sharp batch added

---
+++ src/assets/material_symbols_sharp.bat
@@ -0,0 +1,10 @@
+@echo off
+
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:wght@100
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:wght@200
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:wght@300
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:wght@400
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:wght@500
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:wght@600
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:wght@700
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200
