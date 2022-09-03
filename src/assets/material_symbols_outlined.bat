2613110d56659fb11d12db12337db5a6be97ce4e
lachimi <officiallachimi@gmail.com>
2022-09-03 11:40:10 +0530

    batch added for outlined

---
+++ src/assets/material_symbols_outlined.bat
@@ -0,0 +1,10 @@
+@echo off
+
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@200
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@300
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@400
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@500
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@600
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@700
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200
