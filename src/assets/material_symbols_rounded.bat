5bbd450515b41d7ec361748614236e71068e2153
lachimi <officiallachimi@gmail.com>
2022-09-03 11:43:29 +0530

    rounded batch added

---
+++ src/assets/material_symbols_rounded.bat
@@ -0,0 +1,10 @@
+@echo off
+
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght@100
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght@200
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght@300
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght@400
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght@500
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght@600
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght@700
+start https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200
